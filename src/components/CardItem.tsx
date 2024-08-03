ac7c27f7674f22ba9f8b311be4dbc7c6c186721f
exequiel <exequiel2695@outlook.com>
2024-08-03 17:56:05 -0300

    da

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { BookmarkEmpty, CategoryMovies } from "./icons";
 import { Dot } from "lucide-react";
 import { capitalizeFirstLetter, getYearFromReleaseDate } from "@/lib/utils";
+import Link from "next/link";
 interface Props {
   backdrop_path: string;
   title: string;
@@ -20,37 +21,39 @@ export const CardItem = ({
 }: Props) => {
   return (
     <>
-      <Card className="relative overflow-hidden border-0 bg-inherit">
-        <CardHeader className="absolute z-10 flex w-full flex-row items-center space-y-0 p-2">
-          <Button
-            className="ms-auto rounded-full bg-main-blue-dark/70"
-            size={"icon"}
-          >
-            <BookmarkEmpty />
-          </Button>
-        </CardHeader>
-        <CardContent className="p-0">
-          <Image
-            src={`${TMDB_IMAGE_ENDPOINT}${backdrop_path}`}
-            alt={title}
-            className="h-full w-full rounded-lg object-cover"
-            width={150}
-            height={150}
-            priority
-          />
-        </CardContent>
-        <CardFooter className="flex-col items-start p-0 pt-2 text-white">
-          <div className="flex items-center text-xs font-light md:text-base">
-            <span>{getYearFromReleaseDate(release_date)}</span>
-            <Dot />
-            <div className="flex items-center gap-2">
-              <CategoryMovies />
-              <span>{capitalizeFirstLetter(type)}</span>
+      <Link href={`/movies/${title}`}>
+        <Card className="relative overflow-hidden border-0 bg-inherit">
+          <CardHeader className="absolute z-10 flex w-full flex-row items-center space-y-0 p-2">
+            <Button
+              className="ms-auto rounded-full bg-main-blue-dark/70"
+              size={"icon"}
+            >
+              <BookmarkEmpty />
+            </Button>
+          </CardHeader>
+          <CardContent className="p-0">
+            <Image
+              src={`${TMDB_IMAGE_ENDPOINT}${backdrop_path}`}
+              alt={title}
+              className="h-full w-full rounded-lg object-cover"
+              width={150}
+              height={150}
+              priority
+            />
+          </CardContent>
+          <CardFooter className="flex-col items-start p-0 pt-2 text-white">
+            <div className="flex items-center text-xs font-light md:text-base">
+              <span>{getYearFromReleaseDate(release_date)}</span>
+              <Dot />
+              <div className="flex items-center gap-2">
+                <CategoryMovies />
+                <span>{capitalizeFirstLetter(type)}</span>
+              </div>
             </div>
-          </div>
-          <p>{title}</p>
-        </CardFooter>
-      </Card>
+            <p>{title}</p>
+          </CardFooter>
+        </Card>
+      </Link>
     </>
   );
 };
